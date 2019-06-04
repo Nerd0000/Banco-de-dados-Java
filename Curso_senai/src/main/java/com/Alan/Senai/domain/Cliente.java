76c8db2dab4986269c7e351eef9bb29335ab3c63
Alan C <alancsystem@gmail.com>
2019-06-04 14:36:47 -0300

    adicionado construtor, getters e setters

--- Curso_senai/src/main/java/com/Alan/Senai/domain/Cliente.java
+++ Curso_senai/src/main/java/com/Alan/Senai/domain/Cliente.java
@@ -29,4 +29,93 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name = "Telefone") // nome da tabela vai nas " "
 	private Set<String> telefones = new HashSet<>();
+
+	// Construtor
+	public Cliente() {
+	}
+
+	/**
+	 * @param idCliente
+	 * @param nome
+	 * @param cpfOuCnpj
+	 * @param email
+	 */
+	public Cliente(Integer idCliente, String nome, String cpfOuCnpj, String email) {
+		super();
+		this.idCliente = idCliente;
+		this.nome = nome;
+		this.CpfOuCnpj = cpfOuCnpj;
+		this.email = email;
+	}
+
+	// gets and setters
+	/**
+	 * @return the idCliente
+	 */
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	/**
+	 * @param idCliente the idCliente to set
+	 */
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the cpfOuCnpj
+	 */
+	public String getCpfOuCnpj() {
+		return CpfOuCnpj;
+	}
+
+	/**
+	 * @param cpfOuCnpj the cpfOuCnpj to set
+	 */
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		CpfOuCnpj = cpfOuCnpj;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the telefones
+	 */
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	/**
+	 * @param telefones the telefones to set
+	 */
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
 }
